2a774888631787a6121b17499f69f45c3ecd72e0
Raquel Bayas <31921518+RaquelBayas@users.noreply.github.com>
2023-11-10 09:40:32 +0100

    Deploy2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./**/*.{html,js}"],
+  purge: ["./index.html", "./**/*.{html,js}"],
   theme: {
     extend: {},
   },
